c7c4b1d0e462cc21d45a35b12553554ba43147c5
Jon Borgonia <jon@gomagames.com>
2017-01-02 12:44:46 -1000

    setup initial phaser framework and Game instance
    add Game module
    add Configuration to Game module

---
+++ js/configuration.js
@@ -0,0 +1,11 @@
+(window => {
+  // get or create our Game module
+  window.Game = window.Game || {};
+
+  window.Game.Configuration = {
+    GAME_CONTAINER_ID : 'game-container',
+    GAME_WIDTH : 750,
+    GAME_HEIGHT : 1334,
+  };
+
+})(window);
---
+++ js/main.js
@@ -0,0 +1,17 @@
+(( Phaser, CFG ) => {
+
+  const preload = _ => {
+
+  };
+
+  const create = _ => {
+
+  };
+
+  const update = _ => {
+
+  };
+
+  const game = new Phaser.Game(CFG.GAME_WIDTH, CFG.GAME_HEIGHT, Phaser.AUTO, CFG.GAME_CONTAINER_ID, { preload, create, update });
+
+})(window.Phaser, window.Game.Configuration);
