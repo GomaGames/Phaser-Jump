e3921d3f0a85af50c2c40829da0720751ae6ddfd
Jon Borgonia <jon@gomagames.com>
2017-01-02 16:04:46 -1000

    move hero update into Hero class
    - tweak the movement speeds
    - adjust gravity accel
    - remove collision checks against sides of platforms

--- js/Hero.js
+++ js/Hero.js
@@ -5,7 +5,8 @@
   }
 
   const SCALE = 1;
-  const MOVE_SPEED = 1300;
+  const MOVE_SPEED = 850;
+  const JUMP_VELOCITY = 2950; // also height, gravity:9750 jumpVel:2950 = can clear 400px
   const ANIMATIONS = {
     IDLE_SPEED : 8,
   };
@@ -23,15 +24,36 @@
 
       // allows passing through platforms
       // #TODO this will be bad for checking other objects though
-      this.sprite.body.checkCollision.up = false;
+      this.sprite.body.checkCollision.up =
+      this.sprite.body.checkCollision.left =
+      this.sprite.body.checkCollision.right = false;
 
       // start with idle animation
       this.animations.idle.play(ANIMATIONS.IDLE_SPEED, true);
 
-      this.sprite.update = this.update;
+      this.sprite.update = this.update.bind(this);
     }
 
     update(){
+      let hitPlatform = this.game.physics.arcade.collide(this.sprite, Game.platformsGroup);
+
+      this.sprite.body.velocity.x = 0;
+      if (Game.cursors.left.isDown) {
+        this.sprite.body.velocity.x = -MOVE_SPEED;
+        // player.animations.play('left');
+      } else if (Game.cursors.right.isDown) {
+        this.sprite.body.velocity.x = MOVE_SPEED;
+        // player.animations.play('right');
+      } else {
+        //  Stand still
+        // player.animations.stop();
+      }
+
+      //  Allow the player to jump if they are touching the ground.
+      if (Game.cursors.up.isDown && this.sprite.body.touching.down && hitPlatform)
+      {
+        this.sprite.body.velocity.y = -JUMP_VELOCITY;
+      }
 
     }
   };
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 (( Phaser, Game, CFG ) => {
   let hero;
   let platforms = [];
-  let platformsGroup;
-  let cursors;
+  Game.platformsGroup;
+  Game.cursors = null;
 
   const preload = _ => {
     game.load.atlasJSONHash(CFG.ASSETS.GFX, CFG.ASSETS.ATLAS_PNG_PATH, CFG.ASSETS.ATLAS_JSON_PATH);
@@ -13,51 +13,32 @@
     game.physics.arcade.gravity.y = CFG.GRAVITY;
     game.stage.backgroundColor = CFG.BG_COLOR;
 
-    cursors = game.input.keyboard.createCursorKeys();
+    Game.cursors = game.input.keyboard.createCursorKeys();
 
     // add some platforms
-    platformsGroup = game.add.group();
+    Game.platformsGroup = game.add.group();
 
     let p = new Game.Platform(game, 0, 200, 4);
     platforms.push(p); // gross, fix this
-    platformsGroup.add(p.sprite);
+    Game.platformsGroup.add(p.sprite);
     p = new Game.Platform(game, 550, 880, 4);
     platforms.push(p); // gross, fix this
-    platformsGroup.add(p.sprite);
+    Game.platformsGroup.add(p.sprite);
     p = new Game.Platform(game, 300, 480, 4);
     platforms.push(p); // gross, fix this
-    platformsGroup.add(p.sprite);
+    Game.platformsGroup.add(p.sprite);
 
     p = new Game.Platform(game, 0, 80, 4);
     platforms.push(p); // gross, fix this
-    platformsGroup.add(p.sprite);
+    Game.platformsGroup.add(p.sprite);
     p = new Game.Platform(game, 400, 80, 4);
     platforms.push(p); // gross, fix this
-    platformsGroup.add(p.sprite);
+    Game.platformsGroup.add(p.sprite);
 
     hero = new Game.Hero(game, 500, CFG.GAME_HEIGHT - 200);
   };
 
   const update = _ => {
-    var hitPlatform = game.physics.arcade.collide(hero.sprite, platformsGroup);
-
-    hero.sprite.body.velocity.x = 0;
-    if (cursors.left.isDown) {
-      hero.sprite.body.velocity.x = -150;
-      // player.animations.play('left');
-    } else if (cursors.right.isDown) {
-      hero.sprite.body.velocity.x = 150;
-      // player.animations.play('right');
-    } else {
-      //  Stand still
-      // player.animations.stop();
-    }
-
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && hero.sprite.body.touching.down && hitPlatform)
-    {
-      hero.sprite.body.velocity.y = -1350;
-    }
   };
 
   const game = new Phaser.Game(CFG.GAME_WIDTH, CFG.GAME_HEIGHT, Phaser.AUTO, CFG.GAME_CONTAINER_ID, { preload, create, update });
